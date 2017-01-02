1409f4f959c3b98b8a36b3f08c823715a5c83ffd
nirajpandkar <niraj.pandkar@gmail.com>
2017-01-02 22:20:57 +0530

    All the definitions updated for editing and creating new items

--- ItemCatalog.py
+++ ItemCatalog.py
@@ -15,7 +15,8 @@ session = DBSession()
 @app.route('/categories')
 def show_categories():
     categories = session.query(Category).all()
-    return render_template("categories.html", categories=categories)
+    items = session.query(Item).all()
+    return render_template("categories.html", categories=categories, )
 
 
 @app.route('/category/new', methods=['GET', 'POST'])
@@ -40,11 +41,16 @@ def edit_category(category_name):
         Edits an existing category name.
     '''
     edited_genre = session.query(Category).filter_by(name=category_name)\
-        .one()
+        .first()
     if request.method == 'POST':
         if request.form['edit_category']:
             edited_genre.name = request.form['edit_category']
         session.add(edited_genre)
+        edited_genre_item = session.query(Item).filter_by(
+            category_name=category_name)
+        for i in edited_genre_item:
+            i.category_name = request.form['edit_category']
+            session.add(i)
         session.commit()
         return redirect(url_for('show_categories'))
     else:
@@ -58,31 +64,42 @@ def delete_category(category_name):
 
         Deletes an existing category name.
     '''
-    deleted_genre = session.query(Category).filter_by(name=category_name)
+    deleted_genre = session.query(Category).filter_by(
+        name=category_name)
     if request.method == 'POST':
         deleted_genre.delete(synchronize_session=False)
         session.commit()
         return redirect(url_for('show_categories'))
     else:
         return render_template("deletecategory.html",
-                               genre=deleted_genre.one().name)
+                               genre=deleted_genre.first().name)
 
 
 @app.route('/category/<string:name>/items')
 @app.route('/category/<string:name>/')
 def show_items(name):
     books = session.query(Item).filter_by(category_name=name)
-    return render_template("items.html", books=books)
-
-# @app.route('/category/<string:cateogry_name>/<string:item_name>')
-# def show_particular_item(category_name, item_name):
-#
-#     return render_template("item.html")
+    return render_template("items.html", books=books, genre=name)
 
 
-@app.route('/category/category_name/item/new')
-def new_item():
-    return render_template("newitem.html")
+@app.route('/category/<string:category_name>/<string:item_name>/')
+def show_particular_item(category_name, item_name):
+    genre = session.query(Category).filter_by(name=category_name).first()
+    book = session.query(Item).filter_by(name=item_name).first()
+    return render_template("item.html", book=book, genre=genre)
+
+
+@app.route('/category/<string:category_name>/new', methods=['GET', 'POST'])
+def new_item(category_name):
+    if request.method == 'POST':
+        book = Item(name=request.form['name'],
+                    description=request.form['description'],
+                    category_name=category_name)
+        session.add(book)
+        session.commit()
+        return redirect(url_for('show_items', name=category_name))
+    else:
+        return render_template("newitem.html", category_name=category_name)
 
 
 @app.route('/category/category_name/item/item_name/edit')
