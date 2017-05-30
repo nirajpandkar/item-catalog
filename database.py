2fefa668b15ebd6a83e20a6902a157d752b41f0a
nirajpandkar <niraj.pandkar@gmail.com>
2017-05-30 17:01:52 +0530

    Attribute changes

--- database.py
+++ database.py
@@ -13,7 +13,7 @@ Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
     picture = Column(String(250))
@@ -22,10 +22,10 @@ class User(Base):
 
 class Category(Base):
     __tablename__ = 'category'
-    name = Column(String(80), nullable=False, primary_key=True)
+    name = Column(String(80), primary_key=True, nullable=False)
     id = Column(Integer)
     items = relationship("Item", cascade="all,delete-orphan")
-    user_id = Column(Integer, ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
     user = relationship(User)
 
     @property
@@ -41,12 +41,12 @@ class Item(Base):
     __tablename__ = 'item'
     name = Column(String(200), nullable=False)
     id = Column(Integer, primary_key=True)
-    description = Column(String(250))
+    description = Column(String(500))
 
     category_name = Column(String, ForeignKey('category.name'))
     category = relationship(Category)
 
-    user_id = Column(Integer, ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
     user = relationship(User)
 
     @property
